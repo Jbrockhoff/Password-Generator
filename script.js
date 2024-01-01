5c2c8f75f47d8d92d88a22ada26df204421a4029
Jbrockhoff <150721892+Jbrockhoff@users.noreply.github.com>
2024-01-01 13:36:21 -0600

    rearranged variables, created rule to force client to make at least one selection

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 // Assignment Code
+
+//presented choice arrays under the first variable
 var generateBtn = document.querySelector("#generate");
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numerals = [1234567890];
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+
 
 // Write password to the #password input
+//Created variables that promt users to input numeral(s)
 function generatePassword() {
-    //Created variables for each array
-  var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
-  var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var numerals = [1234567890];
-  var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
   var passwordSelection = [];
   var passwordLength = Number(prompt ("Choose character numer 8-128"));
   
   
-  //Created parameters for character length
+  //Created parameters for character length and rules for no client selection
   if (isNaN(passwordLength) || (passwordLength < 8) || (passwordLength > 128)) {
     alert("Please select a number between 8-128")
+    return;
   }
   else {
     var upperConfirm = confirm("Include uppercase letters?");
@@ -22,6 +26,10 @@ function generatePassword() {
     var numeralsConfirm = confirm("Include numerals?");
     var symbolsConfirm = confirm("Include special characters?");
   }
+  if (!upperConfirm && !lowerConfirm && !numeralsConfirm && !symbolsConfirm) {
+    alert("Please select at least one character type")
+    return;
+  } 
 
   //Created if statements with concat for variables 
   if (upperConfirm) {
@@ -40,11 +48,12 @@ function generatePassword() {
     var passwordSelection = passwordSelection.concat (symbols);
   } 
   
-  var newPassword = []
-  for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+
+  //var newPassword = []
+  //for (let index = 0; index < array.length; index++) {
+    //const element = array[index];
     
-  }
+  //}
 
 }
 function writePassword() {    

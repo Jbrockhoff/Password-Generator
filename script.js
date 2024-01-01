1f7449c1fbca34a4e5363fbe64f6f100fe15ce65
Jbrockhoff <150721892+Jbrockhoff@users.noreply.github.com>
2024-01-01 12:29:31 -0600

    Added for loop- incomplete

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function generatePassword() {
+    //Created variables for each array
   var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
   var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var numerals = [1234567890];
@@ -11,8 +12,7 @@ function generatePassword() {
   var passwordLength = Number(prompt ("Choose character numer 8-128"));
   
   
-  //Created parameters for character length and if statements with concat
-  //for variables 
+  //Created parameters for character length
   if (isNaN(passwordLength) || (passwordLength < 8) || (passwordLength > 128)) {
     alert("Please select a number between 8-128")
   }
@@ -23,6 +23,7 @@ function generatePassword() {
     var symbolsConfirm = confirm("Include special characters?");
   }
 
+  //Created if statements with concat for variables 
   if (upperConfirm) {
     var passwordSelection = passwordSelection.concat (uppercase);
   }
@@ -37,12 +38,15 @@ function generatePassword() {
 
   if (symbolsConfirm) {
     var passwordSelection = passwordSelection.concat (symbols);
+  } 
+  
+  var newPassword = []
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
   }
 
-
 }
-
-
 function writePassword() {    
   var password = generatePassword(); 
   var passwordText = document.querySelector("#password");
